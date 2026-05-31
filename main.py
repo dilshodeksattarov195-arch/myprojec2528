dde55c6269dac79df2732f70069edfa95215c6b7
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-31 12:04:33 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class shippingErocessWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = shippingErocessWorker()
+    print("Worker engine initialized.")
